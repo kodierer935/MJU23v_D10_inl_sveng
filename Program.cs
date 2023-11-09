114426f58a44fba5034c7ea6effc29877513aaa0
Erik Perez <erik.perez.mte23lin@edu.tucsweden.se>
2023-11-09 13:11:14 +0100

    FileNotFoundException line 72 try catch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Net.Mime.MediaTypeNames;
+﻿using System.Linq.Expressions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MJU23v_D10_inl_sveng
 {
@@ -32,125 +33,161 @@ namespace MJU23v_D10_inl_sveng
             //FIXME: programmet kan inte hantera fil som ej hittas.
             string defaultFile = "..\\..\\..\\dict\\sweeng.lis";
             Console.WriteLine("Welcome to the dictionary app!");
+            
             do
+
             {
-                Input("> ");
-                string[] argument = Console.ReadLine().Split();
-                string command = argument[0];
-                // TODO: quit ska avsluta while loopen. (break;)
-                if (command == "quit")
+                try
                 {
-                    Console.WriteLine("Goodbye!");
-                    break;
-                }
-                
-                else if (command == "load")
-                {
-                    if(argument.Length == 2)
+                        Input("> ");
+                    string[] argument = Console.ReadLine().Split();
+                    string command = argument[0];
+                    // TODO: quit ska avsluta while loopen. (break;)
+                    if (command == "quit")
                     {
-                        using (StreamReader sr = new StreamReader(argument[1]))
-                        {
-                            dictionary = new List<SweEngGloss>(); // TODO: Tömma dictionary vid varje load
-                            string line = sr.ReadLine();
-                            while (line != null)
-                            {
-                                SweEngGloss gloss = new SweEngGloss(line);
-                                dictionary.Add(gloss);
-                                line = sr.ReadLine();
-                            }
-                        }
+                        Console.WriteLine("Goodbye!");
+                        break;
                     }
-                    else if(argument.Length == 1)
+                
+                
+                    else if (command == "load")
                     {
-                        using (StreamReader sr = new StreamReader(defaultFile))
-                        {
-                            dictionary = new List<SweEngGloss>(); // TODO: Tömma dictionary vid varje load
-                            string line = sr.ReadLine();
-                            while (line != null)
+                        
+                            if (argument.Length == 2)
                             {
-                                SweEngGloss gloss = new SweEngGloss(line);
-                                dictionary.Add(gloss);
-                                line = sr.ReadLine();
+                                try 
+                                {
+                                using (StreamReader sr = new StreamReader(argument[1]))
+                                {
+                                    dictionary = new List<SweEngGloss>(); // TODO: Tömma dictionary vid varje load
+                                    string line = sr.ReadLine();
+                                    while (line != null)
+                                    {
+                                        SweEngGloss gloss = new SweEngGloss(line);
+                                        dictionary.Add(gloss);
+                                        line = sr.ReadLine();
+                                    }
+                                }
+                                }
+                                catch (FileNotFoundException) { Console.WriteLine("THIS FILE NOT FOUND!"); }
+
+                                
                             }
-                        }
+
+
+
+
+
+                            else if (argument.Length == 1)
+                            {
+
+                                using (StreamReader sr = new StreamReader(defaultFile))
+                                {
+                                    dictionary = new List<SweEngGloss>(); // TODO: Tömma dictionary vid varje load
+                                    string line = sr.ReadLine();
+                                    while (line != null)
+                                    {
+                                        SweEngGloss gloss = new SweEngGloss(line);
+                                        dictionary.Add(gloss);
+                                        line = sr.ReadLine();
+                                    }
+                                }
+
+
+                            }
+                    
+                    
+                    
+
                     }
                     // NYI: felhantering för filer som ej hittas
-                }
-                else if (command == "list")
-                {
-                    //FIXME: felhantering för tom lista eller ingen vald fil
-                    foreach(SweEngGloss gloss in dictionary)
+
+                    else if (command == "list")
                     {
-                        Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}");
-                    }
-                }
-                else if (command == "new")
-                {
-                    //FIXME: filhantering eller separat lista för nya ord utan att ladda fil. 
-                    if (argument.Length == 3)
-                    {
-                        dictionary.Add(new SweEngGloss(argument[1], argument[2]));
-                    }
-                    //FIXME: felhantering ifall endast ett ord är skrivet efter kommandot "new" t.ex. new sol.
-                    else if(argument.Length == 1)
-                    {
-                        string swe = Input("Write word in Swedish: ");
-                        string eng = Input("Write word in English: ");
-                        dictionary.Add(new SweEngGloss(swe, eng));
-                    }
-                }
-                else if (command == "delete")
-                {
-                    if (argument.Length == 3)
-                    {
-                        int index = -1;
-                        for (int i = 0; i < dictionary.Count; i++) {
-                            SweEngGloss gloss = dictionary[i];
-                            if (gloss.word_swe == argument[1] && gloss.word_eng == argument[2])
-                                index = i;
-                        }
-                        //FIXME: felhantering om ordet inte finns i ordlistan.
-                        dictionary.RemoveAt(index);
-                    }
-                    else if (argument.Length == 1)
-                    {
-                        string del_swe = Input("Write word in Swedish: ");
-                        string del_eng = Input("Write word in English: ");
-                        int index = -1;
-                        for (int i = 0; i < dictionary.Count; i++)
+                         //FIXME: felhantering för tom lista eller ingen vald fil
+                        foreach (SweEngGloss gloss in dictionary)
                         {
-                            SweEngGloss gloss = dictionary[i];
-                            if (gloss.word_swe == del_swe && gloss.word_eng == del_eng)
-                                index = i;
+                            Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}");
                         }
-                        dictionary.RemoveAt(index);
+                    
                     }
-                }
-                else if (command == "translate")
-                {
-                    string userInput;
-                    if (argument.Length == 2)
-                        userInput = argument[1];
-                    if (argument.Length == 1)
+                    else if (command == "new")
                     {
-                        userInput = Input("Write word to be translated: ");
+                   
+                        //FIXME: filhantering eller separat lista för nya ord utan att ladda fil. 
+                        if (argument.Length == 3)
+                        {
+                            dictionary.Add(new SweEngGloss(argument[1], argument[2]));
+                        }
+                        //FIXME: felhantering ifall endast ett ord är skrivet efter kommandot "new" t.ex. new sol.
+                        else if (argument.Length == 1)
+                        {
+                            string swe = Input("Write word in Swedish: ");
+                            string eng = Input("Write word in English: ");
+                            dictionary.Add(new SweEngGloss(swe, eng));
+                        }
+                   
+                    
                     }
-                    foreach (SweEngGloss gloss in dictionary)
+                    else if (command == "delete")
                     {
-                        if (gloss.word_swe == userInput)
-                            Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
-                        if (gloss.word_eng == userInput)
-                            Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
-                    } 
+                        if (argument.Length == 3)
+                        {
+                            int index = -1;
+                            for (int i = 0; i < dictionary.Count; i++) {
+                                SweEngGloss gloss = dictionary[i];
+                                if (gloss.word_swe == argument[1] && gloss.word_eng == argument[2])
+                                    index = i;
+                            }
+                            //FIXME: felhantering om ordet inte finns i ordlistan.
+                            dictionary.RemoveAt(index);
+                        }
+                        else if (argument.Length == 1)
+                        {
+                            string del_swe = Input("Write word in Swedish: ");
+                            string del_eng = Input("Write word in English: ");
+                            int index = -1;
+                            for (int i = 0; i < dictionary.Count; i++)
+                            {
+                                SweEngGloss gloss = dictionary[i];
+                                if (gloss.word_swe == del_swe && gloss.word_eng == del_eng)
+                                    index = i;
+                            }
+                            dictionary.RemoveAt(index);
+                        }
+                    }
+                    else if (command == "translate")
+                    {
+                        string userInput;
+                        if (argument.Length == 2)
+                            userInput = argument[1];
+                        if (argument.Length == 1)
+                        {
+                            userInput = Input("Write word to be translated: ");
+                        }
+                        foreach (SweEngGloss gloss in dictionary)
+                        {
+                            if (gloss.word_swe == userInput)
+                                Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
+                            if (gloss.word_eng == userInput)
+                                Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
+                        } 
                      
-                    //NYI: felhantering om ordet inte finns i ordlistan.
+                        //NYI: felhantering om ordet inte finns i ordlistan.
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Unknown command: '{command}'");
+                    }
                 }
-                else
+                catch (Exception e)
                 {
-                    Console.WriteLine($"Unknown command: '{command}'");
+                    Console.WriteLine(e.Message);
                 }
             }
             while (true);
+            
+            
         }
     }
 }
